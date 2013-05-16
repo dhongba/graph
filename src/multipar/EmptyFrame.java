7e9651ec4e04023221fc5b0339dde459ff12e105
Tashi Sherpa <dhongba@yahoo.com>
2013-05-16 15:39:53 +0200

    project created

---
+++ src/multipar/EmptyFrame.java
@@ -0,0 +1,21 @@
+package multipar;
+
+import javax.swing.*;
+
+class MyFrame extends JFrame {
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+public MyFrame() {
+	setTitle("My Empty Frame");
+	setSize(300,200); // default size is 0,0
+	setLocation(10,200); // default is 0,0 (top left corner)
+  }
+
+//public static void main(String[] args) {
+//    JFrame f = new MyFrame();
+//    f.setVisible(true);
+//  }
+}
